37bd745345ca033f39db8d5616376460477224fa
zzangzwh1 <116697810+zzangzwh1@users.noreply.github.com>
2022-10-25 21:06:45 -0600

    Add files via upload

---
+++ RE_ICA13/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GDIDrawer;
+
+namespace RE_ICA13
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+    public class MyRandom : Random
+    {
+        private int maxSize;
+        public MyRandom(int maxSize)
+        {
+            this.maxSize = maxSize;
+        }
+        public Rectangle NextDrawerRect(CDrawer gdi)
+        {
+
+            return new Rectangle();
+        }
+    
+    }
+  
+
+
+}
